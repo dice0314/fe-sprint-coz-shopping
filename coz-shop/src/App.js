79b29977915641b92cd8583edf908a0dd2d42ee2
dice0314 <dice0314@naver.com>
2023-07-11 16:36:41 +0900

    feat: 컴포넌트, 페이지 파일 생성 [#1]

--- coz-shop/src/App.js
+++ coz-shop/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Main from './page/Main';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Main></Main>
         <p>
           초기 세팅 테스트
         </p>
