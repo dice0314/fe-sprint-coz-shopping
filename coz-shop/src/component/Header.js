a60542bb6af0a2dc46e04a3b9b8a44d68f4d8700
dice0314 <dice0314@naver.com>
2023-07-11 18:40:36 +0900

    feat: 헤더 컴포넌트 제작[2]

--- coz-shop/src/component/Header.js
+++ coz-shop/src/component/Header.js
@@ -3,7 +3,11 @@ import './header.css'
 export default function Header() {
     return (
         <header className="header-container">
-            <img src="logo.png" alt="logo" />
+            <div className="logo-container">
+                <img src="logo.png" alt="logo" />
+                <span className='logo-title'>COZ Shopping</span>
+            </div>
+            <img src="hamburger.png" alt="hamburger" />
         </header>
     )
 }
