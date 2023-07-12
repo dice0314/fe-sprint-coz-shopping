3d06507f7a477c1fa455e0fd52bfd92a77f64cf3
dice0314 <dice0314@naver.com>
2023-07-12 10:35:36 +0900

    feat: 푸터 컴포넌트 제작[3]

---
+++ coz-shop/src/component/Footer.js
@@ -0,0 +1,12 @@
+import './footer.css'
+
+export default function Footer() {
+    return (
+        <footer className="footer-container">
+            <div className="info-container">
+                <p>개인정보 처리방침 | 이용 약관</p>
+                <p>All rights reserved @ Codestates</p>
+            </div>
+        </footer>
+    )
+}
