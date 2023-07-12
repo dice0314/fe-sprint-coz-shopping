c94dd089dfabda4d78ae23193dd9be5bff294ed9
dice0314 <dice0314@naver.com>
2023-07-12 14:07:14 +0900

    fear: 상품컴포넌트 기본 틀 제작[9]

--- coz-shop/src/page/Main.js
+++ coz-shop/src/page/Main.js
@@ -1,10 +1,12 @@
 import Footer from "../component/Footer"
 import Header from "../component/Header"
+import Item from "../component/Item"
 
 export default function Main() {
     return (
         <section>
             <Header></Header>
+            <Item></Item>
             <Footer></Footer>
         </section>
     )
