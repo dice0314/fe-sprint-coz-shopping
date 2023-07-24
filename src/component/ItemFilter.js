fd268ff8b584f72f9ce03f60e63592275d9e8466
dice0314 <dice0314@naver.com>
2023-07-24 15:25:22 +0900

    feat: useRef로 변경 및 ItemFilter 리팩토링

--- src/component/ItemFilter.js
+++ src/component/ItemFilter.js
@@ -1,29 +1,23 @@
-import './itemFilter.css'
+import React from 'react';
+import './itemFilter.css';
 
-export default function ItemFilter({ handleFilterClick }) {
+const filterOptions = [
+  { name: '전체', img: '/image_component/All.png', type: 'All' },
+  { name: '상품', img: '/image_component/Product.png', type: 'Product' },
+  { name: '카테고리', img: '/image_component/Category.png', type: 'Category' },
+  { name: '기획전', img: '/image_component/Exhibition.png', type: 'Exhibition' },
+  { name: '브랜드', img: '/image_component/Brand.png', type: 'Brand' },
+];
 
-    return (
-        <div className="itemFilter-container">
-            <div className="filter-all" onClick={handleFilterClick}>
-                <img src="/image_component/All.png" alt="logo" />
-                <span className="filter-name itemFilter-select">전체</span>
-            </div>
-            <div className="filter-product" onClick={handleFilterClick}>
-                <img src="/image_component/Product.png" alt="logo" />
-                <span className="filter-name">상품</span>
-            </div>
-            <div className="filter-category" onClick={handleFilterClick}>
-                <img src="/image_component/Category.png" alt="logo" />
-                <span className="filter-name">카테고리</span>
-            </div>
-            <div className="filter-exhibition" onClick={handleFilterClick}>
-                <img src="/image_component/Exhibition.png" alt="logo" />
-                <span className="filter-name">기획전</span>
-            </div>
-            <div className="filter-brand" onClick={handleFilterClick}>
-                <img src="/image_component/Brand.png" alt="logo" />
-                <span className="filter-name">브랜드</span>
-            </div>
+export default function ItemFilter({ handleFilterClick, itemFilterRef, filterType }) {
+  return (
+    <div className="itemFilter-container">
+      {filterOptions.map((option) => (
+        <div key={option.type} className="filter-option" onClick={() => handleFilterClick(option.type)}>
+          <img src={option.img} alt={option.name} />
+          <span ref={itemFilterRef} className={filterType === option.type ? "filter-name itemFilter-select" : "filter-name"}>{option.name}</span>
         </div>
-    )
+      ))}
+    </div>
+  );
 }
